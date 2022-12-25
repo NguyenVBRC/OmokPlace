12f04d2369b4f8b1ed63153d0918ecedb708ee40
Jimmy Nguyen <nguyen89.mke@gmail.com>
2022-12-24 21:15:57 -0800

    Removed Public URI cause that's not smart

--- server/index.js
+++ server/index.js
@@ -1,25 +1,39 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
 const app = express();
 
-const uri = "mongodb+srv://Test123:TestDeezCandice123@cluster0.ujggnnh.mongodb.net/?retryWrites=true&w=majority"
+app.use(cors());
+app.use(express.json());
+
+const uri =
+  "mongodb+srv://Test123:@cluster0.ujggnnh.mongodb.net/?retryWrites=true&w=majority";
 
 const accountSchema = {
-    name: String,
-    password: String
-}
+  name: String,
+  password: String,
+};
+
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+
+app.post("/signup", function (req, res) {
+  console.log(req.body.username, req.body.password);
+  res.end();
+});
 
 async function connect() {
-    try {
-        await mongoose.connect(uri)
-        console.log("Connected to MongoDb")
-    } catch(error) {
-        console.error(error)
-    }
+  try {
+    await mongoose.connect(uri);
+    console.log("Connected to MongoDb");
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 connect();
 
-app.listen(8000, ()=> {
-    console.log("Server started on port 8000")
+app.listen(8000, () => {
+  console.log("Server started on port 8000");
 });
