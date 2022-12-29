15e44d249956ea5b14c561a0633abcb645657c8d
nguyen89mke <nguyen89.mke@gmail.com>
2022-12-29 05:14:57 -0800

    Client Connected to Server

--- server/index.js
+++ server/index.js
@@ -2,16 +2,23 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const app = express();
+// const dotenv = require('dotenv')
+
+// import * as dotenv from "dotenv";
+// require("dotenv").config();
+
+// DB_KEY = process.env.MONGO_KEY
 
 app.use(cors());
 app.use(express.json());
 
 const uri =
-  "mongodb+srv://Test123:TestDeezCandice123@cluster0.ujggnnh.mongodb.net/?retryWrites=true&w=majority";
+  `mongodb+srv://Test123:@cluster0.ujggnnh.mongodb.net/?retryWrites=true&w=majority`;
 
 const accountSchema = {
   name: String,
   password: String,
+  confirmPass: String,
 };
 
 app.get("/", (req, res) => {
@@ -19,7 +26,7 @@ app.get("/", (req, res) => {
 });
 
 app.post("/signup", function (req, res) {
-  console.log(req);
+  console.log(req.body.username, req.body.password, req.body.confirmPass);
   res.end();
 });
 
